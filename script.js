f1b443189adfc119f1a6c0da59dd68de52f8c560
abdulmaleekadams <harbdulmaleek.com>
2023-09-28 15:55:50 +0100

    add script

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-const menuButton = document.getElementById('open-menu')
-const navContainer = document.querySelector('.navContainer')
-const requestBtnContainer  = document.querySelector('.requestBtnContainer ')
+const menuButton = document.getElementById('open-menu');
+const navContainer = document.querySelector('.navContainer');
+const requestBtnContainer = document.querySelector('.requestBtnContainer ');
+
+let isMenuOpen = false;
+
+function closeMenu() {
+    navContainer.classList.remove('show');
+    requestBtnContainer.classList.remove('show');
+    menuButton.parentElement.classList.remove('close');
+    isMenuOpen = false;
+}
 
 menuButton.addEventListener('click', () => {
-   navContainer.classList.toggle('show')
-    requestBtnContainer.classList.toggle('show')
-    menuButton.parentElement.classList.toggle('close')
-})
+  if (!isMenuOpen) {
+    navContainer.classList.add('show');
+    requestBtnContainer.classList.add('show');
+    menuButton.parentElement.classList.add('close');
+    isMenuOpen = true;
+  } else {
+    closeMenu()
+  }
+});
 
+document.body.addEventListener('click', (event) => {
+  // Check if the click occurred outside of the menu
+  if (isMenuOpen && !menuButton.contains(event.target)) {
+    closeMenu();
+  }
+});
